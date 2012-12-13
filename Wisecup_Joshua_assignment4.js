9258e38bbf7b262854450c74bffaf302e76be700
mistafi <mistafi@gmail.com>
2012-12-12 23:59:33 -0500

    Setting up a few more.

--- Wisecup_Joshua_assignment4.js
+++ Wisecup_Joshua_assignment4.js
@@ -2,17 +2,20 @@
 
 
 function phoneChecker( phone ) {
-  phoneRegex = /^\(\d\d\d\) \d\d\d-\d\d\d\d$/;
- if( !phone.match( phoneRegex ) ) {
-  alert( 'Please enter a valid phone number' );
-  return false;
- }
- return true;
+	phoneRegex = /^\(\d{3}\) \d{3}-\d{4}$/; 
+	 if( !phone.match( phoneRegex ) ) {
+	  alert( 'Please enter a valid phone number' );
+	  return false;
+	 }
+	return true;
 }
 
-
-
-phoneRegex = /^\(\d{3}\) \d{3}-\d{4}$/; 
-
-
 function emailChecker (){}
+
+function urlChecker (){}
+
+function stringCase (){}
+
+function stringSeparator (){}
+
+
