e7e0875e8b8c8062fc28373735969712e336c407
mistafi <mistafi@gmail.com>
2012-12-12 23:17:43 -0500

    Added first function

--- Wisecup_Joshua_assignment4.js
+++ Wisecup_Joshua_assignment4.js
@@ -1,1 +1,11 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+
+function phoneChecker( phone ) {
+  phoneRegex = /^\(\d\d\d\) \d\d\d-\d\d\d\d$/;
+ if( !phone.match( phoneRegex ) ) {
+  alert( 'Please enter a valid phone number' );
+  return false;
+ }
+ return true;
+}
