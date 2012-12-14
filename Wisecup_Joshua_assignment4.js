a4a6bfdb330699f99276aa23422dca1402a1f7d9
mistafi <mistafi@gmail.com>
2012-12-13 23:12:47 -0500

    Updates to the functions.

--- Wisecup_Joshua_assignment4.js
+++ Wisecup_Joshua_assignment4.js
@@ -2,13 +2,13 @@
 
 // String Functions
 
+
 function phoneChecker( phone ) {
 	phoneRegex = /^\(\d{3}\) \d{3}-\d{4}$/; 
 		if( !phone.match( phoneRegex ) ) {
 			alert( 'Please enter a valid phone number.' );
 		return false;
 		}
-	console.log( 'The phone number is valid.' );
 	return true;
 }
 
@@ -18,7 +18,6 @@ function emailChecker( email ){
   			alert( 'Please enter a valid email address.' );
   		return false;
  		}
-	console.log( 'The email is valid.' );
  	return true;
 }
 
@@ -28,28 +27,38 @@ function urlChecker( url ) {
   			alert( 'Please enter a valid URL.' );
   		return false;
  		}
-	console.log( 'The URL is valid.' );
  	return true;
 }
 
 
 function stringCase ( string ) {  
 	return string.toLowerCase().replace(/\b(\w)/g, function ( $1 ) { return $1.toUpperCase(); });
-  	console.log( 'The characters have been converted to uppercase.' );
 }  
 
 
 function stringSeparator ( dateString ){
 	var replaceRegex = /([01]?\d)[-\/ .\/ ,]([0123]?\d)[-\/ .\/ ,](\d{4})/;
  		if( !dateString.replace( replaceRegex, '$1/$2/$3' ) ) {
-  			console.log( 'The characters have been replaced.' );
  		}
 }
 
 
 // Number Functions
 
-function moneyMaker (){}
+
+function moneyChecker() {
+	var moneyValidation = document.getElementById("money").value;
+	var pattern = /^\d+(?:\.\d{0,2})$/ ;
+	if (pattern.test(moneyValidation)) {
+		alert("The funds are in valid format.");
+		return true;
+	} 
+		alert("The funds are not in valid format. Please enter funds in 00.00 format.");
+		return false;
+
+}
+
+
 
 function percentDif (){}
 
